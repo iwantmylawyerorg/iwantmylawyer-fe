2303d91e936efec905942f272ce484289bad31e2
batikanyeni <batikanyeni@outlook.com>
2024-04-14 19:56:04 +0300

    signup visual update

--- src/pages/login-page/login-page.component.ts
+++ src/pages/login-page/login-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MinimalHeaderComponent} from "../../headers/minimal-header/minimal-header.component";
 import {NgStyle} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [
     MinimalHeaderComponent,
-    NgStyle
+    NgStyle,
+    RouterLink
   ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
--- src/pages/signup-page/signup-page.component.ts
+++ src/pages/signup-page/signup-page.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NgClass} from "@angular/common";
+import {ClientSignUpFormComponent} from "../../forms/client-sign-up-form/client-sign-up-form.component";
+import {LawyerSignUpFormComponent} from "../../forms/lawyer-sign-up-form/lawyer-sign-up-form.component";
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgClass,
+    ClientSignUpFormComponent,
+    LawyerSignUpFormComponent
+  ],
   templateUrl: './signup-page.component.html',
   styleUrl: './signup-page.component.css'
 })
 export class SignupPageComponent {
+  signUpType:String = "Client";
 
+  clientSignUpClicked() {
+    this.signUpType = "Client";
+  }
+  lawyerSignUpClicked() {
+    this.signUpType = "Lawyer";
+  }
 }
