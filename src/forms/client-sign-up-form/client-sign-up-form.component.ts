3fe342da831763efd605baf1971659c227e04e61
batikanyeni <batikanyeni@outlook.com>
2024-04-15 16:50:02 +0300

    signup done

--- src/forms/client-sign-up-form/client-sign-up-form.component.ts
+++ src/forms/client-sign-up-form/client-sign-up-form.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-client-sign-up-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    NgClass
+  ],
   templateUrl: './client-sign-up-form.component.html',
   styleUrl: './client-sign-up-form.component.css'
 })
 export class ClientSignUpFormComponent {
+  agreedToTerms: boolean = false;
 
+
+ handleButtonStatus(){
+  this.agreedToTerms = !this.agreedToTerms;
+ }
 }
--- src/forms/lawyer-sign-up-form/lawyer-sign-up-form.component.ts
+++ src/forms/lawyer-sign-up-form/lawyer-sign-up-form.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-lawyer-sign-up-form',
   standalone: true,
-  imports: [],
+    imports: [
+        FormsModule
+    ],
   templateUrl: './lawyer-sign-up-form.component.html',
   styleUrl: './lawyer-sign-up-form.component.css'
 })
 export class LawyerSignUpFormComponent {
+  agreedToTerms: boolean = false;
 
+
+  handleButtonStatus(){
+    this.agreedToTerms = !this.agreedToTerms;
+  }
 }
