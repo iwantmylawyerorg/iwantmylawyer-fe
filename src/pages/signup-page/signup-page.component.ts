da4b8535b14b476ca752657c2b0f9d2fc48f0311
batikanyeni <batikanyeni@outlook.com>
2024-04-17 12:47:14 +0300

    signup-page animation added

--- src/pages/signup-page/signup-page.component.ts
+++ src/pages/signup-page/signup-page.component.ts
@@ -3,6 +3,9 @@ import {RouterLink} from "@angular/router";
 import {NgClass} from "@angular/common";
 import {ClientSignUpFormComponent} from "../../forms/client-sign-up-form/client-sign-up-form.component";
 import {LawyerSignUpFormComponent} from "../../forms/lawyer-sign-up-form/lawyer-sign-up-form.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { trigger, transition, style, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-signup-page',
@@ -11,10 +14,22 @@ import {LawyerSignUpFormComponent} from "../../forms/lawyer-sign-up-form/lawyer-
     RouterLink,
     NgClass,
     ClientSignUpFormComponent,
-    LawyerSignUpFormComponent
+    LawyerSignUpFormComponent,
   ],
   templateUrl: './signup-page.component.html',
-  styleUrl: './signup-page.component.css'
+  styleUrl: './signup-page.component.css',
+  animations: [
+    trigger('slideIn', [
+      transition('client => lawyer', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.3s ease-in-out', style({ transform: 'translateX(0%)' }))
+      ]),
+      transition('lawyer => client', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.3s ease-in-out', style({ transform: 'translateX(0%)' }))
+      ])
+    ])
+  ]
 })
 export class SignupPageComponent {
   signUpType:String = "Client";
