51bde2b31c9b426757acc12c3b9550be426f3288
batikanyeni <batikanyeni@outlook.com>
2024-04-19 14:13:54 +0300

    lawyer-page-half-done

--- src/app/cards/lawyer-card/lawyer-card.component.spec.ts
+++ src/app/cards/lawyer-card/lawyer-card.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MinimalHeaderComponent } from './minimal-header.component';
+import { LawyerCardComponent } from './lawyer-card.component';
 
-describe('MinimalHeaderComponent', () => {
-  let component: MinimalHeaderComponent;
-  let fixture: ComponentFixture<MinimalHeaderComponent>;
+describe('LawyerCardComponent', () => {
+  let component: LawyerCardComponent;
+  let fixture: ComponentFixture<LawyerCardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MinimalHeaderComponent]
+      imports: [LawyerCardComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(MinimalHeaderComponent);
+    fixture = TestBed.createComponent(LawyerCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/cards/lawyer-card/lawyer-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lawyer-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './lawyer-card.component.html',
+  styleUrl: './lawyer-card.component.css'
+})
+export class LawyerCardComponent {
+
+}
